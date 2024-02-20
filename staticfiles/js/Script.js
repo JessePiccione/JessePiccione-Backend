5d3cfe4740b7f2d8c82dc049b31dd1e434c1f4f1
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-02-20 17:27:35 -0500

    added in some stuff to create a working text alert service

--- staticfiles/js/Script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', loadHomeView);
-
-async function loadHomeView(event){
-    event.preventDefault();
-    const response =  await fetch('/home')
-    const main = document.getElementById('mainContentContainer').innerHTML = await response.text()
-}
