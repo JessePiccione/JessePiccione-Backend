555b3677f77ba66cdc27f8623b2cb6da2d3ed40f
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-02-20 17:27:35 -0500

    added in some stuff to create a working text alert service

--- Message/views.py
+++ Message/views.py
@@ -1,9 +1,10 @@
 from django.core.exceptions import ValidationError
 from django.views import View
+from twilio.rest import Client 
 
 from Resume.views import index
 from .forms import MessageForm
-
+from .twiliocreds import *
 
 # Create your views here.
 class MessageView(View):
@@ -11,6 +12,10 @@ class MessageView(View):
         f = MessageForm(request.POST)
         if(f.is_valid()):
             f.save()
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(from_='whatsapp:+18447953913', 
+                                             body=f'''New Message from {request.POST['name']}\nEmail: {request.POST['email']}\nSubject: {request.POST['subject']}\nMessage:\n{request.POST['message']}''',
+                                             to='whatsapp:+17329080037')
             return index(request)
         raise ValidationError()
                 
