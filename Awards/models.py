617bcead6036018f79c7df6fad6658be408418ed
Jesse Piccione <72052332+JessePiccione@users.noreply.github.com>
2024-11-05 18:31:08 +0000

    added some nullable fields

--- Awards/models.py
+++ Awards/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from datetime import datetime 
 class AwardCategory(models.Model):
     title = models.CharField(max_length=100)
-    year = models.IntegerField(default=datetime.now().year, null=True)
-    issuer = models.CharField(max_length=255, null=True)
+    year = models.IntegerField(default=datetime.now().year, null=True, blank=True)
+    issuer = models.CharField(max_length=255, null=True, blank=True)
     def __str__(self):
         return f"[{self.title}]"
 class Award(models.Model):
