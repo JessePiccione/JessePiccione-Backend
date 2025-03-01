be4d9a2f27d8209244dfc7027852cea171993217
Jesse Piccione <jessepiccione@yahoo.com>
2025-03-01 19:42:34 +0000

    created backendinterface getting ready to strip out old database and migrate to new unified cloud project

--- Message/models.py
+++ Message/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Message(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, default='')
     subject = models.CharField(max_length=255)
     message = models.TextField()
     def __str__(self):
