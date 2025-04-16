56a885a7d6bc1426df90cef5ca1518cbf606fb36
Jesse Piccione <jessepiccione@yahoo.com>
2025-04-16 14:02:36 -0400

    css admin statics

--- ResumeAPI/admin.py
+++ ResumeAPI/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Award, AwardCategory
 # Register your models here.
+
+class AwardAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'year', 'category')
+    search_fields = ('id','title', 'year', 'category')
+    list_filter = ('title', 'year', 'category')
+admin.site.register(Award, AwardAdmin)
